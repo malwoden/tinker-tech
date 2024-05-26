e2d119b4b2bcfe83de5481dab13f9f6b7bf1980c
malwoden <neewill@gmail.com>
2024-05-26 17:32:13 +0100

    Bootstrap the tf state bucket for the network account

---
+++ opentofu/network/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "tinker-tech-network-tf-state"
+    key            = "network.tfstate"
+    profile        = "network"
+    region         = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-2"
+  profile = "network"
+}
---
+++ opentofu/network/s3.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_account_public_access_block" "this" {
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "tinker-tech-network-tf-state"
+}
